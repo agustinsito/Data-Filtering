94918de8021a06e438e5e2ccd224fcb4c93063cd
Agustin Manetta <agustinmanetta8@gmail.com>
2023-02-02 17:47:13 -0300

    v1

---
+++ app/script.js
@@ -0,0 +1,28 @@
+const input = document.querySelector('#searchInput')
+const userList = document.querySelector('#users')
+
+let users = []
+
+window.addEventListener('DOMContentLoaded', async () => {
+    userList.innerHTML = "<small>loading... </small>"
+    const data = await loadUsers();
+    users = data.data
+    renderUsers(users)
+})
+
+async function loadUsers (){
+    const response = await fetch('https://fakerapi.it/api/v1/users?_quantity=1000')
+    return await response.json()
+}
+
+input.addEventListener('keyup', e => {
+    const newUsers = users.filter(user=> `${user.firstname.toLowerCase()} ${user.lastname.toLowerCase()}`.includes(input.value.toLowerCase()))
+    renderUsers(newUsers)
+})
+
+const createUserItems = users => users.map(user => `<li class="bgUsers bgUsers:hover">${user.firstname} ${user.lastname}</li>`).join('')
+
+function renderUsers(users){
+    const itemsString = createUserItems (users)
+    userList.innerHTML = itemsString;
+}
